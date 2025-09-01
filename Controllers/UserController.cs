caaf3e8c275093811d0fd0c15c12b6f00979c52c
NikollbibajNoah <nikollbibaj.noah@gmail.com>
2025-09-01 16:22:23 +0200

    Controller fixes

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -20,60 +20,47 @@ public class UserController : ControllerBase
     }
 
     [HttpGet]
-    // [Authorize]
+    [Authorize]
     public async Task<ActionResult<List<User>>> GetAllUsers()
     {
         var users = await _userService.GetAllUsers();
-        // if (users == null || users.Count == 0) return NoContent();
 
         return Ok(users);
     }
     
     [HttpGet("{id}")]
-    // [Authorize]
+    [Authorize]
     public async Task<ActionResult<User>> GetUserById(int id)
     {
         var foundUser = await _userService.GetUserById(id);
 
-        // if (foundUser == null) throw new NotFoundException($"User with ID {id} was not found.");
-        
         return Ok(foundUser);
     }
 
     [HttpPost]
-    // [Authorize(Roles = nameof(UserRole.Admin))]
+    [Authorize(Roles = nameof(UserRole.Admin))]
     public async Task<ActionResult<User>> CreateUser(User user)
     {
-        // if (!ModelState.IsValid) throw new ValidationException("User model is invalid.");
-
         var createdUser = await _userService.CreateUser(user);
 
         return CreatedAtAction(nameof(GetUserById), new { id = createdUser.Id }, createdUser); // 201
     }
 
     [HttpPut("{id}")]
-    // [Authorize(Roles = nameof(UserRole.Admin))]
+    [Authorize(Roles = nameof(UserRole.Admin))]
     public async Task<ActionResult<User>> UpdateUser(int id, User user)
     {
-        // if (id != user.Id) throw new ValidationException($"Given ID {id} does not match User ID {user.Id}.");
-        //
-        // if (!ModelState.IsValid) throw new ValidationException("User model is invalid.");
-        
         var updatedUser = await _userService.UpdateUser(id, user);
 
-        // if (updatedUser == null) return NotFound();
-        
         return Ok(updatedUser);
     }
 
     [HttpDelete("{id}")]
-    // [Authorize(Roles = nameof(UserRole.Admin))]
+    [Authorize(Roles = nameof(UserRole.Admin))]
     public async Task<ActionResult<User>> DeleteUser(int id)
     {
         var deletedUser = await _userService.DeleteUser(id);
         
-        // if (deletedUser == null) throw new NotFoundException($"User with ID {id} was not found.");
-
         return Ok(deletedUser);
     }
 }
